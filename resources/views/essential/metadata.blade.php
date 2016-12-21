0e7ac245ec0da9da2f5cddb921a3afc76151b47d
raoasifraza11 <raoasifraza1@hotmail.com>
2016-12-22 00:03:07 +0500

    fix css styling

--- resources/views/essential/metadata.blade.php
+++ resources/views/essential/metadata.blade.php
@@ -1,25 +1,15 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="TheFoody is a food ordering platform which brings restaurants and food lovers together. Food ordering online is easier than any other platforms.">
-	<meta name="keywords" content="food, order online, restaurant, reservation, book a table, foodies, cafe, recipes, menu, dishes, chefs and cooking experts ">
+	<meta name="description" content="The University admission plateform that provide you all currently open admission of institute.">
+	<meta name="keywords" content="University, Institute, Open Admission, Technology, Engineering, Information Technology">
 	<meta name="author" content="iglyphic">
 	<link rel="shortcut icon" href="img/favicon.png">
 	
-	<!-- reset CSS -->
-	<link href="{{ URL::asset('css/reset.css') }}" rel="stylesheet">
-	<!-- Bootstrap css -->
-	<link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-
-	<!-- icon CSS -->
-	<link href="{{ URL::asset('css/fontello.css') }}" rel="stylesheet" type="text/css">
-	<link href="{{ URL::asset('css/font-awesome.min.css') }}" rel="stylesheet">
-
-	<!-- Main css -->
-	<link href="{{ URL::asset('css/main.css') }}" rel="stylesheet" type="text/css">
-	<link href="{{ URL::asset('css/style4.css') }}" rel="stylesheet" type="text/css">
-
-	
+	<!-- CSS -->
+	@foreach(glob("css/*.css") as $css)
+		<link type="text/css" rel="stylesheet" href="{{$css}}">
+	@endforeach
 
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
